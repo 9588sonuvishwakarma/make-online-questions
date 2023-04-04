81f7ecbe0b0babd3e7087261ef1da487ea7ab8ff
sonu <sonuvishwa021@gmail.com>
2023-04-04 18:27:44 +0530

    done

---
+++ hl.py
@@ -0,0 +1,12 @@
+print("Question 1 => write a program to print hello world ")
+print("Question 2 => write a program to make calculator ")
+value = input("type here")
+
+with open("sonu.text","a") as op:
+    op.write(str([str ])+ ":" +value+ "\n")
+    print("thank you \n your answer is saved ")
+
+values = input("enter second answer\n")
+with open("sonu.text","a") as op:
+    op.write(str([str ])+ ":" +values+ "\n")
+    print("thank you \n your answer is saved ")
